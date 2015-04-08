2e83cfeeed398b76f806f6e25af2deddce3e2ec1
Peter Cai <xqsx43cxy@126.com>
2015-04-08 23:29:08 +0800

    Hook method to ensure package name is changed

--- app/src/main/java/net/typeblog/webview/ModWebView.java
+++ app/src/main/java/net/typeblog/webview/ModWebView.java
@@ -3,6 +3,9 @@ package net.typeblog.webview;
 import android.content.res.XResources;
 
 import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XC_MethodHook;
+import static de.robv.android.xposed.XposedHelpers.*;
 
 public class ModWebView implements IXposedHookZygoteInit
 {
@@ -10,6 +13,14 @@ public class ModWebView implements IXposedHookZygoteInit
 	@Override
 	public void initZygote(IXposedHookZygoteInit.StartupParam startupParam) throws Throwable {
 		XResources.setSystemWideReplacement("android", "string", "config_webViewPackageName", "com.google.android.webview");
+		
+		findAndHookMethod("android.webkit.WebViewFactory", null, "getWebViewPackageName", new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+				XposedBridge.log("getWebViewPackageName");
+				param.setResult("com.google.android.webview");
+			}
+		});
 	}
 
 }
